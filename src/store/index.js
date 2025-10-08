04d4613448d1c2faff527f05222e712bda354f3c
lupinus <bzirs@outlook.com>
2025-10-08 21:22:37 +0800

    :new: feat: prettier

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,25 @@
 const initialState = {
   balance: 0,
   loan: 0,
-  loanPurpose: '',
+  loanPurpose: "",
   isLoading: false,
 };
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'account/deposit':
-      return {...state, balance: action.balance + action.payload};
-    case 'account/withdraw':
-      return {...state, balance: action.balance - action.payload};
-    case 'account/requestLoan':
+    case "account/deposit":
+      return { ...state, balance: action.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: action.balance - action.payload };
+    case "account/requestLoan":
       if (state.loan > 0) return state;
       // todo
-      return {...state, loan: action.payload};
-    case 'account/payLoan':
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
       return {
         ...state,
         loan: 0,
-        loanPurpose: '',
+        loanPurpose: "",
         balance: state.balance - state.loan,
       };
     default:
