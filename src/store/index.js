40b519e1cbb5d84153498a0c9f3ff57d1c78301a
lupinus <bzirs@outlook.com>
2025-10-09 19:41:14 +0800

    :new: feat: customer reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,19 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: "",
+  nationID: "",
+  createAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -33,26 +39,104 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationID: action.payload.nationID,
+        createAt: action.payload.createAt,
+      };
 
-store.dispatch({
-  type: "account/deposit",
-  payload: 200,
+    case "customer/updateCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+      };
+
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
 });
 
+const store = createStore(rootReducer);
+
+// store.dispatch({
+//   type: "account/deposit",
+//   payload: 200,
+// });
+//
+// console.log(store.getState());
+//
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: {
+//     amount: 1000,
+//     loanPurpose: "车贷",
+//   },
+// });
+//
+// console.log(store.getState());
+// store.dispatch({
+//   type: "account/payLoan",
+// });
+//
+// console.log(store.getState());
+
+// action
+function deposit(amount) {
+  return {
+    type: "account/deposit",
+    payload: amount,
+  };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, loanPurpose) {
+  return {
+    type: "account/requestLoan",
+    payload: {
+      amount,
+      loanPurpose,
+    },
+  };
+}
+
+function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
+
+store.dispatch(deposit(300));
 console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: {
-    amount: 1000,
-    loanPurpose: "车贷",
-  },
-});
+function createCustomer(fullName, nationID) {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName,
+      nationID,
+      createAt: new Date().toLocaleString(),
+    },
+  };
+}
 
-console.log(store.getState());
-store.dispatch({
-  type: "account/payLoan",
-});
+function updateCustomerName(fullName) {
+  return {
+    type: "customer/updateCustomerName",
+    payload: { fullName },
+  };
+}
 
+store.dispatch(createCustomer("lupinus", "wozuiniubi"));
 console.log(store.getState());
