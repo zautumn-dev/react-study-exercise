c5f9caf30a969e080ee8b0b9a5382a1d8fe673a7
lupinus <bzirs@outlook.com>
2025-10-09 19:18:40 +0800

    :new: feat: 尝试 redux

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "./store/index";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -8,21 +10,49 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: action.balance + action.payload };
+      return { ...state, balance: state.balance + action.payload };
     case "account/withdraw":
       return { ...state, balance: action.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      // todo
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.loanPurpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
+        balance: state.balance - state.loan,
         loan: 0,
         loanPurpose: "",
-        balance: state.balance - state.loan,
       };
     default:
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({
+  type: "account/deposit",
+  payload: 200,
+});
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: {
+    amount: 1000,
+    loanPurpose: "车贷",
+  },
+});
+
+console.log(store.getState());
+store.dispatch({
+  type: "account/payLoan",
+});
+
+console.log(store.getState());
